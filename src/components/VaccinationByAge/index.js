7462790b430b24f59a3cc9f338e3b223786532e1
AkhilReddy588 <akhilreddya654@gmail.com>
2024-05-31 04:16:58 +0000

    changes

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -55,32 +55,30 @@ class VaccinationByAge extends Component {
       if (number === 0) {
         return number
       }
-      return number * 1500 + 'k'
+      return `${number * 1500}k`
     }
     return (
       <div>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie
-              cx="70%"
-              cy="40%"
-              data={vaccinationByAGe}
-              startAngle={0}
-              endAngle={360}
-              dataKey="count"
-            >
-              <Cell name="18-44" fill="#fecba6" />
-              <Cell name="45-60" fill="#b3d23f" />
-              <Cell name="Above 60" fill="#a44c9e" />
-            </Pie>
-            <Legend
-              iconType="circle"
-              layout="vertical"
-              verticalAlign="middle"
-              align="right"
-            />
-          </PieChart>
-        </ResponsiveContainer>
+        <PieChart width="100%" height={300}>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={vaccinationByAGe}
+            startAngle={0}
+            endAngle={360}
+            dataKey="count"
+          >
+            <Cell name="18-44" fill="#fecba6" />
+            <Cell name="45-60" fill="#b3d23f" />
+            <Cell name="Above 60" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="vertical"
+            verticalAlign="middle"
+            align="right"
+          />
+        </PieChart>
       </div>
     )
   }
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -55,34 +55,32 @@ class VaccinationByGender extends Component {
       if (number === 0) {
         return number
       }
-      return number * 1500 + 'k'
+      return `${number * 1500}k`
     }
     return (
       <div>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie
-              cx="70%"
-              cy="40%"
-              data={vaccinationByGender}
-              startAngle={0}
-              endAngle={180}
-              innerRadius="40%"
-              outerRadius="70%"
-              dataKey="count"
-            >
-              <Cell name="Male" fill="#fecba6" />
-              <Cell name="Female" fill="#b3d23f" />
-              <Cell name="Others" fill="#a44c9e" />
-            </Pie>
-            <Legend
-              iconType="circle"
-              layout="vertical"
-              verticalAlign="middle"
-              align="right"
-            />
-          </PieChart>
-        </ResponsiveContainer>
+        <PieChart width="100%" height={300}>
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={vaccinationByGender}
+            startAngle={0}
+            endAngle={180}
+            innerRadius="40%"
+            outerRadius="70%"
+            dataKey="count"
+          >
+            <Cell name="Male" fill="#fecba6" />
+            <Cell name="Female" fill="#b3d23f" />
+            <Cell name="Others" fill="#a44c9e" />
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="vertical"
+            verticalAlign="middle"
+            align="right"
+          />
+        </PieChart>
       </div>
     )
   }
